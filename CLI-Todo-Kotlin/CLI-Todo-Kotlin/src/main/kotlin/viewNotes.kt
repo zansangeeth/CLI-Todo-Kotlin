d8d6eb7f53047dde206af4b3da5952c1634e2cb1
Sangeeth Amirthanathan <zansangeeth17@gmail.com>
2026-06-20 04:07:31 +0100

    fixed the table view

--- CLI-Todo-Kotlin/CLI-Todo-Kotlin/src/main/kotlin/viewNotes.kt
+++ CLI-Todo-Kotlin/CLI-Todo-Kotlin/src/main/kotlin/viewNotes.kt
@@ -4,13 +4,27 @@ import kotlin.io.println
 
 
 fun vewNotes() {
+
+    val db = notesDBInstance.notesDB
+
+
+    val idMaxWidth = maxOf("ID".length, db.keys.maxOfOrNull { it.length } ?: 0)
+    val noteWidth = maxOf(
+        "Note".length,
+        db.values.flatten().maxOfOrNull { it.getNote.length } ?: 0
+    )
+    val dateMaxWidth = maxOf(
+        "Note".length,
+        db.values.flatten().maxOfOrNull { it.getNote.length } ?: 0
+    )
+
+    println("| %-${idMaxWidth}s | %-${noteWidth}s | %-${dateMaxWidth}s |".format("ID", "Note", "Time"))
     notesDBInstance.notesDB.forEach { (id, notes) ->
-        println("ID: $id")
+
         notes.forEach { note ->
-            println("  Note: ${note.getNote}")
-            println("  Date: ${note.getTime}")
-            println()
+            println("| ${id.padEnd(idMaxWidth)} | ${note.getNote.padEnd(noteWidth)} | ${note.getTime.padEnd(dateMaxWidth)} |")
         }
+
     }
     cliInit()
 }
